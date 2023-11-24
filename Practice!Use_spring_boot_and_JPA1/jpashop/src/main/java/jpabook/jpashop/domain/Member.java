d411911bdcbd47d7920fa32a6277bd9706ce6f05
seungwoo byeon <mongoose@yu.ac.kr>
2023-11-24 14:39:36 +0000

    TIL: 231124

---
+++ Practice!Use_spring_boot_and_JPA1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+    
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    @Embedded
+    private Address address;
+
+    // 주인 필드 값에 mapping 됐다!
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
