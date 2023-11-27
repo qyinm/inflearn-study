3b9992a3008043677a58a0f8001094cee5b3fc85
seungwoo byeon <mongoose@yu.ac.kr>
2023-11-27 06:05:11 +0000

    feat: item business logic and custom exception

--- Practice!Use_spring_boot_and_JPA1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ Practice!Use_spring_boot_and_JPA1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,4 +32,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // stock 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // stock 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고수량이 부족합니다.");
+        }
+        this.stockQuantity = restStock;
+    }
 }
