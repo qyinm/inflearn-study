df0b43089377ecde9cb4016d9ea558d076ae7cbb
seungwoo <mongoose@yu.ac.kr>
2024-08-07 19:09:07 +0900

    project creation

---
+++ src/main/java/me/qyinm/demoinflearnrestapi/DemoInflearnRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.qyinm.demoinflearnrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInflearnRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoInflearnRestApiApplication.class, args);
+    }
+
+}
