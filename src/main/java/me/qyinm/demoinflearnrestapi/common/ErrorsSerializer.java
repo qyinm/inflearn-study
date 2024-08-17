a586c19b8daeefe36c89c41b2bfa6f658491ade3
seungwoo <mongoose@yu.ac.kr>
2024-08-17 15:52:58 +0900

    To check Body for BadRequest

---
+++ src/main/java/me/qyinm/demoinflearnrestapi/common/ErrorsSerializer.java
@@ -0,0 +1,43 @@
+package me.qyinm.demoinflearnrestapi.common;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.validation.Errors;
+
+import java.io.IOException;
+// provided by spring, so that we can use it as a bean serializer
+@JsonComponent
+public class ErrorsSerializer extends JsonSerializer<Errors> {
+    @Override
+    public void serialize(Errors errors, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeStartArray();
+        errors.getFieldErrors().forEach(e -> {
+            try {
+                jsonGenerator.writeStartObject();
+                jsonGenerator.writeStringField("field", e.getField());
+                jsonGenerator.writeStringField("objectName", e.getObjectName());
+                jsonGenerator.writeStringField("code", e.getCode());
+                jsonGenerator.writeStringField("defaultMessage", e.getDefaultMessage());
+                Object rejectedValue = e.getRejectedValue();
+                if (rejectedValue != null) {
+                    jsonGenerator.writeStringField("rejectedValue", rejectedValue.toString());
+                }
+                jsonGenerator.writeEndObject();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        errors.getGlobalErrors().forEach(e -> {
+            try {
+                jsonGenerator.writeStringField("objectName", e.getObjectName());
+                jsonGenerator.writeStringField("code", e.getCode());
+                jsonGenerator.writeStringField("defaultMessage", e.getDefaultMessage());
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        jsonGenerator.writeEndArray();
+    }
+}
--- src/main/java/me/qyinm/demoinflearnrestapi/events/EventController.java
+++ src/main/java/me/qyinm/demoinflearnrestapi/events/EventController.java
@@ -35,12 +35,12 @@ public class EventController {
     // if EventDto is not valid, errors will be filled with error messages
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
 
         eventValidator.validate(eventDto, errors);
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
