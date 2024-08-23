9f2a2761d3b8e954be8c9ce019024ec6087ac11f
qyinm <qusseun@gmail.com>
2024-08-23 18:22:13 +0900

    Build spring rest docs

--- src/main/java/me/qyinm/demoinflearnrestapi/events/EventController.java
+++ src/main/java/me/qyinm/demoinflearnrestapi/events/EventController.java
@@ -2,6 +2,7 @@ package me.qyinm.demoinflearnrestapi.events;
 
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -52,6 +53,7 @@ public class EventController {
         EventResource eventResource = new EventResource(event);
         eventResource.add(linkTo(EventController.class).withRel("query-events"));
         eventResource.add(selfLinkBuilder.withRel("update-event"));
+        eventResource.add(Link.of("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
     }
 }
--- src/test/java/me/qyinm/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/qyinm/demoinflearnrestapi/events/EventControllerTests.java
@@ -68,14 +68,12 @@ public class EventControllerTests {
                 .andExpect(jsonPath("free").value(false))
                 .andExpect(jsonPath("offline").value(true))
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
-                .andExpect(jsonPath("_links.self").exists())
-                .andExpect(jsonPath("_links.query-events").exists())
-                .andExpect(jsonPath("_links.update-event").exists())
                 .andDo(document("create-event",
                         links(halLinks(),
                                 linkWithRel("self").description("link to self"),
                                 linkWithRel("query-events").description("link to query events"),
-                                linkWithRel("update-event").description("link to update an existing")
+                                linkWithRel("update-event").description("link to update an existing"),
+                                linkWithRel("profile").description("link to profile")
                         ),
                         requestHeaders(
                                 headerWithName(HttpHeaders.ACCEPT).description("accept header"),
@@ -114,8 +112,12 @@ public class EventControllerTests {
                                 fieldWithPath("free").description("it tells if this event is free or not"),
                                 fieldWithPath("offline").description("it tells if this event is offline or not"),
                                 fieldWithPath("eventStatus").description("event status"),
-                                subsectionWithPath("_links").description("it's links complying HATEOAS SPEC")
-                        )
+//                                subsectionWithPath("_links").description("it's links complying HATEOAS SPEC")
+                                fieldWithPath("_links.self.href").description("link to self"),
+                                fieldWithPath("_links.query-events.href").description("link to query-events"),
+                                fieldWithPath("_links.update-event.href").description("link to update-event"),
+                                fieldWithPath("_links.profile.href").description("link to profile")
+                                )
                 ))
         ;
 
