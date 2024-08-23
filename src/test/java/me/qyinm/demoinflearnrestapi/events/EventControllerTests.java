cab0f56e1a1bb829c35d395dedab51280a5d6d20
qyinm <qusseun@gmail.com>
2024-08-23 19:39:16 +0900

    Connect postges executed on Docker

--- src/test/java/me/qyinm/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/qyinm/demoinflearnrestapi/events/EventControllerTests.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
+@ActiveProfiles("test")
 @Import(RestDocsConfiguration.class)
 public class EventControllerTests {
 
