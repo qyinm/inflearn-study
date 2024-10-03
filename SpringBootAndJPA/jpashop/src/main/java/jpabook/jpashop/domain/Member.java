5ae79978a5e1f5a8351b93deb14215fe64da7ed3
qyinm <qusseun@gmail.com>
2024-10-03 22:45:23 +0900

    file re-organized and add restapi with DTO

--- SpringBootAndJPA/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ SpringBootAndJPA/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,6 +21,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
