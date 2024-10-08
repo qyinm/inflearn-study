ca40a5e31b4732eb7a5d3e5f61001ea0beaa1b69
qyinm <qusseun@gmail.com>
2024-10-08 13:19:53 +0900

    open session in view, command query pattern

--- SpringBootAndJPA/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ SpringBootAndJPA/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,22 +1,20 @@
 package jpabook.jpashop.api;
 
-import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
-import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
-import lombok.Getter;
+import jpabook.jpashop.service.query.OrderDto;
+import jpabook.jpashop.service.query.OrderQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.util.stream.Collectors.*;
@@ -27,6 +25,7 @@ public class OrderApiController {
 
     private final OrderRepository orderRepository;
     private final OrderQueryRepository orderQueryRepository;
+    private final OrderQueryService orderQueryService;
     
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
@@ -51,13 +50,7 @@ public class OrderApiController {
 
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
-        List<Order> orders = orderRepository.findAllWithItem();
-        for (Order order : orders) {
-            System.out.println("order ref: " + order + " order id: " + order.getId());
-        }
-        return orders.stream()
-                .map(OrderDto::new)
-                .toList();
+        return orderQueryService.orderV3();
     }
 
     @GetMapping("/api/v3.1/orders")
@@ -97,43 +90,4 @@ public class OrderApiController {
                 .collect(toList());
     }
 
-    @Getter
-    static class OrderDto {
-
-        private Long orderId;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
-        private List<OrderItemDto> orderItems;
-
-        public OrderDto(Order order) {
-
-            orderId = order.getId();
-            name = order.getMember().getName(); // LAZY 초기화
-            orderDate = order.getOrderDate();
-            orderStatus = order.getStatus();
-//            order.getOrderItems().stream()
-//                    .forEach(o -> o.getItem().getName());
-            // orderitem 이 entity 그 자체로 들어감
-            address = order.getDelivery().getAddress(); // LAZY 초기화
-            orderItems = order.getOrderItems().stream()
-                    .map(OrderItemDto::new)
-                    .toList();
-        }
-    }
-
-    @Getter
-    static class OrderItemDto {
-
-        private String itemName;
-        private int orderPrice;
-        private int count;
-
-        public OrderItemDto(OrderItem orderItem) {
-            itemName = orderItem.getItem().getName();
-            orderPrice = orderItem.getOrderPrice();
-            count = orderItem.getCount();
-        }
-    }
 }
