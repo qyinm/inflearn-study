a9335dc363a7cd10ef0858ede2d2d9c17bf202a6
qyinm <qusseun@gmail.com>
2024-10-14 00:01:18 +0900

    use spring data jpa & query method

--- SpringDataJPA/src/main/java/com/hippoo/springdatajpa/repository/MemberRepository.java
+++ SpringDataJPA/src/main/java/com/hippoo/springdatajpa/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.hippoo.springdatajpa.repository;
 import com.hippoo.springdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
--- SpringDataJPA/src/main/java/com/hippoo/springdatajpa/repository/TeamRepository.java
+++ SpringDataJPA/src/main/java/com/hippoo/springdatajpa/repository/TeamRepository.java
@@ -1,41 +1,7 @@
 package com.hippoo.springdatajpa.repository;
 
-import com.hippoo.springdatajpa.entity.Member;
 import com.hippoo.springdatajpa.entity.Team;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll() {
-        return em.createQuery("select t from Team t", Team.class)
-                .getResultList();
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long count() {
-        return em.createQuery("select count(t) from Member t", Long.class)
-                .getSingleResult();
-    }
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
