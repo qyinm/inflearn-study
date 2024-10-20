48cc91062d7931b807cc1b4f047d019b8076cb55
qyinm <qusseun@gmail.com>
2024-10-20 13:21:38 +0900

    @Query 정적쿼리 생성

--- SpringDataJPA/src/test/java/com/hippoo/springdatajpa/repository/MemberRepositoryTest.java
+++ SpringDataJPA/src/test/java/com/hippoo/springdatajpa/repository/MemberRepositoryTest.java
@@ -91,4 +91,16 @@ class MemberRepositoryTest {
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(m1);
     }
+
+    @Test
+    public void testQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 10);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(m1);
+    }
 }
