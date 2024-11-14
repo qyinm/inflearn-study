79c52b6b01496596fe5f70d4c6440999d8e3d2ba
qyinm <qusseun@gmail.com>
2024-11-14 23:20:23 +0900

    domain converter 실습

---
+++ SpringDataJPA/src/main/java/com/hippoo/springdatajpa/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.hippoo.springdatajpa.controller;
+
+import com.hippoo.springdatajpa.entity.Member;
+import com.hippoo.springdatajpa.repository.MemberRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+
+}
