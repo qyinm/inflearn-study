cc7abe9232cdf7d76b5c8a7fe5d94977145bd7a0
qyinm <qusseun@gmail.com>
2024-11-16 23:04:23 +0900

    spring data jpa 저장 구조 persist, merge

--- SpringDataJPA/src/main/java/com/hippoo/springdatajpa/controller/MemberController.java
+++ SpringDataJPA/src/main/java/com/hippoo/springdatajpa/controller/MemberController.java
@@ -35,11 +35,12 @@ public class MemberController {
                 .map(MemberDto::new);
     }
 
+    /**
     @PostConstruct
     public void init() {
         for (int i = 0; i < 100; i++) {
             memberRepository.save(new Member("user" + i, i));
         }
     }
-
+    */
 }
--- SpringDataJPA/src/main/java/com/hippoo/springdatajpa/entity/BaseTimeEntity.java
+++ SpringDataJPA/src/main/java/com/hippoo/springdatajpa/entity/BaseTimeEntity.java
@@ -2,9 +2,11 @@ package com.hippoo.springdatajpa.entity;
 
 import jakarta.persistence.Column;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+@Getter
 public class BaseTimeEntity {
 
     @CreatedDate
