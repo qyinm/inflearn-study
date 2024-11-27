6adc4aa31a344d2dbc67a60223b5c75de0688c11
qyinm <qusseun@gmail.com>
2024-11-27 17:48:14 +0900

    test: querydsl test

--- QueryDsl/src/test/java/com/hippoo/querydsl/QueryDslApplicationTests.java
+++ QueryDsl/src/test/java/com/hippoo/querydsl/QueryDslApplicationTests.java
@@ -1,13 +1,36 @@
 package com.hippoo.querydsl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.hippoo.querydsl.entity.Hello;
+import com.hippoo.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QueryDslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query.selectFrom(qHello).fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
